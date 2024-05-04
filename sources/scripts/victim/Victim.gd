91e0b61db54dfc6c41c10767dd22320c72c1fb59
cianid <cianid@kali.kali>
2024-05-04 19:50:18 +0300

    some changes in victim grabbing

--- sources/scripts/victim/Victim.gd
+++ sources/scripts/victim/Victim.gd
@@ -4,14 +4,24 @@ extends KinematicBody2D
 var fsm: VictimStateMachine
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+const SPEED = 80
+const DRAGGING_SPEED = 10
+const GRAVITY = 15
+const JUMPFORCE = -500
+const SPEED_SLIDE = 70
+const DRAG_OFFSET = 12
+
+var velocity = Vector2(0,0) # Скорость игрока по координатам x, y
+
+onready var sprite = get_node("PlayerSprite")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	velocity = move_and_slide(velocity, Vector2.UP) # Vector2.UP - направелние снизу вверх. Внизу пол.
+	velocity.y +=  GRAVITY
+	velocity.x = lerp(velocity.x, 0, 0.3)
+
+	
 
 
 
@@ -24,11 +34,21 @@ func _on_Player_is_grabbing_victim():
 	fsm.change_to('panic')
 
 
-func _on_Player_is_dragging_victim():
+func _on_Player_is_dragging_victim(player):
+	self.position.x = calculate_position_by_player(player)
+	self.sprite.flip_h = not player.sprite.flip_h
 	fsm.change_to('fear')
+	
 
 
-func _on_Player_change_position(x, y):
-	self.position.x = x-12
-	self.position.y = y
-	print(x,y, 'POSITION')
+func _on_Player_change_position(player):
+	self.position.x = calculate_position_by_player(player)
+
+
+func calculate_position_by_player(player):
+	return player.position.x-DRAG_OFFSET * player.current_flip_to_num()
+
+
+
+func _on_Player_is_not_victim_grabbing():
+	fsm.change_to('idle')

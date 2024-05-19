c75b5c18f1e0dc8599b6ca08c786681f6b80b9e0
cianid <cianid@kali.kali>
2024-05-19 16:38:52 +0300

    Добавлены слезы для жертвы

--- sources/scripts/victim/fuck.gd
+++ sources/scripts/victim/fuck.gd
@@ -7,13 +7,18 @@ var curr_z_index: int
 func enter():
 	curr_z_index = fsm.player_root.z_index
 	fsm.player_root.z_index=3
+	fsm.player_root.tears_particles.toogle_tears_particle(true)
+	fsm.player_root.tears_particles.set_tears_particle_by_direction(fsm.player_root._get_direction_num())
+	
 	fsm.player.play('fuck')
 	
 
 
 func exit(next_state):
 	fsm.player_root.z_index=curr_z_index
+	fsm.player_root.tears_particles.toogle_tears_particle(false)
 	fsm.change_to(next_state)
+
 
 
 func process(_delta):
